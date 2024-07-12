0f7a1bd5acabc58387546adb41ae5d4766758e97
NAVENDRA SINGH <raghvsinghtomar1975@gmail.com>
2024-07-12 22:32:35 +0530

    Issue resolved in logger file

--- src/logger.py
+++ src/logger.py
@@ -2,6 +2,12 @@ import logging
 import os
 from datetime import datetime
 
+'''
+    logger file is like a journal that helps you keep track of what your code is doing, 
+    making it easier to debug issues, monitor performance, and understand how 
+    your application behaves over time.
+'''
+
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
